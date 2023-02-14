dc7a3813c63d81fa95922554aa6064c7811855a7
Michael Angelo De Leon <iammikedev@gmail.com>
2023-02-15 04:37:44 +0800

    refactors user interface based on the design

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <link rel="icon" href="{{ url('build/assets/img/favicon.ico')}}" type="image/x-icon">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -13,17 +15,17 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans antialiased text-gray-900">
+        <div class="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <x-application-logo class="h-40 fill-current text-gray-500" />
+                    <x-application-logo class="h-40 text-gray-500 fill-current" />
                 </a>
             </div>
 
-            <h1 class="mt-5">SBCI LAN-Based Library System Catalogue</h1>
+            <h1 class="mt-5 text-2xl">SBCI LAN-Based Library System Catalogue</h1>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white sm:max-w-xs dark:bg-gray-800">
                 {{ $slot }}
             </div>
         </div>
