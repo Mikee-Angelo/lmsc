4c2687b23f73dd4ab0caab37a5aadb0852f665a5
Michael Angelo De Leon <iammikedev@gmail.com>
2023-02-15 19:58:10 +0800

    refactors placement of elements based on the design

--- resources/views/components/jet-bar-stat-card.blade.php
+++ resources/views/components/jet-bar-stat-card.blade.php
@@ -1,22 +1,30 @@
 @php
     $colors = [
-            'info' => 'text-blue-500 bg-blue-100 dark:text-blue-100 dark:bg-blue-500',
-            'success' => 'text-green-500 bg-green-100 dark:text-green-100 dark:bg-green-500',
-            'warning' => 'text-yellow-500 bg-yellow-100 dark:text-orange-100 dark:bg-orange-500',
-            'danger' => 'text-red-500 bg-red-100 dark:text-red-100 dark:bg-red-500',
+            'info' => 'bg-blue-700',
+            'success' => 'bg-emerald-700',
+            'warning' => 'bg-yellow-700',
+            'danger' => 'bg-rose-700',
     ];
 @endphp
 
-<div class="flex items-center p-4 bg-white border rounded-lg shadow-xs dark:bg-gray-800">
-    <div class="p-3 mr-4 rounded-full {{ $colors[$type ?? 'info'] }}">
-        {{ $slot }}
-    </div>
-    <div>
-        <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+<div {{ $attributes->merge(['class' => "flex items-center p-4 rounded-md shadow-xs dark:bg-gray-800 " . $colors[$type ?? 'info']])}}>
+    
+    <div class="w-full">
+        <div class="flex flex-row items-start justify-between mb-3">
+            <p class="text-3xl font-semibold text-white">
+                {{ $number }}
+            </p>
+
+            <div class="p-3 mr-4 text-white">
+                {{ $slot }}
+            </div>
+        </div>
+        
+
+        <p class="mb-2 text-sm text-white">
             {{ $title }}
         </p>
-        <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-            {{ $number }}
-        </p>
     </div>
+
+    
 </div>
