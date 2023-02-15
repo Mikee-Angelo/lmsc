c143137b7ef389b98c4b1aef5779639c1017cc71
Michael Angelo De Leon <iammikedev@gmail.com>
2023-02-15 20:00:30 +0800

    refactors attribute of application logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ url('/img/logo.png') }}" alt="Subic Bay Colleges, Inc. Logo" {{ $attributes->merge(['class' => 'h-40']) }}>
+<img src="{{ url('/img/logo.png') }}" alt="Subic Bay Colleges, Inc. Logo" {{ $attributes }}>
