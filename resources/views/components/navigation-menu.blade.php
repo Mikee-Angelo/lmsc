3fd8ce9b6f25c20742400ea0fe56a87379ae92f7
Michael Angelo De Leon <iammikedev@gmail.com>
2023-02-15 19:22:08 +0800

    implements jetbar package on navigation links

---
+++ resources/views/components/navigation-menu.blade.php
@@ -0,0 +1,142 @@
+<header class="items-center justify-end hidden px-6 py-4 bg-white border-gray-200 lg:flex">
+
+    <div class="flex items-center">
+        {{-- Notifications --}}
+        <div x-data="{ notificationOpen: false }" class="relative">
+            <button @click="notificationOpen = ! notificationOpen"
+                    class="flex mx-4 text-gray-600 focus:outline-none">
+                <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                </svg>
+            </button>
+
+            <div x-show="notificationOpen" @click="notificationOpen = false" class="fixed inset-0 z-10 w-full h-full" style="display: none;"></div>
+
+            <div x-show="notificationOpen" class="absolute right-0 z-10 mt-2 overflow-hidden bg-white border rounded-lg shadow-xl w-80" style="width: 20rem; display: none;">
+                <a href="#" class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:bg-gray-100">
+                    <figure class="w-1/6">
+                        <img class="object-cover w-8 h-8 mx-1 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80" alt="avatar">
+                    </figure>
+                    <p class="w-full mx-2 text-sm">
+                        <span class="font-bold" href="#">Sara Salah</span> replied on the <span class="font-bold text-indigo-400">Upload Image</span> article. 2m
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:bg-gray-100">
+                    <figure class="w-1/6">
+                        <img class="object-cover w-8 h-8 mx-1 rounded-full" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80" alt="avatar">
+                    </figure>
+                    <p class="w-full mx-2 text-sm">
+                        <span class="font-bold" href="#">Slick Net</span> start following you . 45m
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:bg-gray-100">
+                    <figure class="w-1/6">
+                        <img class="object-cover w-8 h-8 mx-1 rounded-full" src="https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80" alt="avatar">
+                    </figure>
+                    <p class="w-full mx-2 text-sm">
+                        <span class="font-bold" href="#">Jane Doe</span> Like Your reply on <span class="font-bold text-indigo-400" href="#">Test with TDD</span> artical . 1h
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:bg-gray-100">
+                    <figure class="w-1/6">
+                        <img class="object-cover w-8 h-8 mx-1 rounded-full" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=398&amp;q=80" alt="avatar">
+                    </figure>
+                    <p class="w-full mx-2 text-sm">
+                        <span class="font-bold" href="#">Abigail Bennett</span> start following you . 3h
+                    </p>
+                </a>
+            </div>
+        </div>
+        {{-- End Notifications --}}
+
+        {{-- User Dropdown --}}
+
+        <div x-data="{ dropdownOpen: false }" class="relative">
+
+            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                <button @click="dropdownOpen = ! dropdownOpen" class="flex text-sm transition duration-150 ease-in-out border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
+                    <img class="object-cover w-8 h-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                </button>
+            @else
+                <span class="inline-flex rounded-md">
+                    <button @click="dropdownOpen = ! dropdownOpen" type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none">
+                        {{ Auth::user()->name }}
+
+                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </span>
+            @endif
+
+            <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 z-10 w-full h-full" style="display: none;"></div>
+
+            <div x-show="dropdownOpen"
+                 class="absolute right-0 z-10 w-48 mt-2 overflow-hidden bg-white border rounded-md shadow-xl"
+                 style="display: none;">
+
+                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Manage Team') }}
+                    </div>
+
+                    <!-- Team Settings -->
+                    <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                        {{ __('Team Settings') }}
+                    </x-dropdown-link>
+
+                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                        <x-dropdown-link href="{{ route('teams.create') }}">
+                            {{ __('Create New Team') }}
+                        </x-dropdown-link>
+                    @endcan
+
+                    <div class="border-t border-gray-100"></div>
+
+                    <!-- Team Switcher -->
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Switch Teams') }}
+                    </div>
+
+                    @foreach (Auth::user()->allTeams() as $team)
+                        <x-switchable-team :team="$team" />
+                    @endforeach
+
+                    <div class="border-t border-gray-100"></div>
+                @endif
+
+                <!-- Account Management -->
+                <div class="block px-4 py-2 text-xs text-gray-400">
+                    {{ __('Manage Account') }}
+                </div>
+
+                <x-dropdown-link href="{{ route('profile.show') }}">
+                    {{ __('Profile') }}
+                </x-dropdown-link>
+
+                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                    <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                        {{ __('API Tokens') }}
+                    </x-dropdown-link>
+                @endif
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-dropdown-link href="{{ route('logout') }}"
+                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-dropdown-link>
+                </form>
+            </div>
+
+        </div>
+        {{-- End User Dropdown --}}
+
+    </div>
+</header>
