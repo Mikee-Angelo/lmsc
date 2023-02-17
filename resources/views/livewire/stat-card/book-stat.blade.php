052ab8b4c28eb28ccd494d1fbc5dfa10dab47bc5
Michael Angelo De Leon <iammikedev@gmail.com>
2023-02-17 15:33:37 +0800

    adds livewire component for total number of books

---
+++ resources/views/livewire/stat-card/book-stat.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- Do your work, then step back. --}}
+    <x-jet-bar-stat-card title="No. of Books" number="{{ $count }}" type="info">
+        <x-jet-bar-icon type="users" fill />
+    </x-jet-bar-stat-card>
+</div>
