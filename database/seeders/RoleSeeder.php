710f4dd16aa210bbe4d27e190cbf59b4d39dd913
Michael Angelo De Leon <iammikedev@gmail.com>
2023-04-13 12:38:02 +0800

    adds new role with permissions

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        $role = Role::create(['name' => 'Admin']); 
+
+        $p0 = Permission::create(['name' => 'create books']);
+        $p1 = Permission::create(['name' => 'edit books']); 
+        $p2 = Permission::create(['name' => 'delete books']);
+        $p3 = Permission::create(['name' => 'update books']);
+        Permission::create(['name' => 'create account']);
+        Permission::create(['name' => 'disable account']);
+        Permission::create(['name' => 'delete account']);
+        Permission::create(['name' => 'update account']);
+        
+        $role->givePermissionTo(Permission::all());
+
+        $assistantRole = Role::create(['name' => 'Assistant Librarian']);
+        
+        $assistantRole->givePermissionTo([$p0, $p1, $p2,  $p3]);
+    }
+}
