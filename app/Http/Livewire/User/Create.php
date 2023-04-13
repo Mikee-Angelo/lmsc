28947d73a8af47e4784bed94ff73d5616dc8ceae
Michael Angelo De Leon <iammikedev@gmail.com>
2023-04-13 12:37:41 +0800

    adds create module

--- app/Http/Livewire/User/Create.php
+++ app/Http/Livewire/User/Create.php
@@ -4,12 +4,33 @@ namespace App\Http\Livewire\User;
 
 use Livewire\Component;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
 
 class Create extends Component
 {
     public User $user;
-    public $confirmingUserCreate = false; 
+   
+    //Handles all the list of roles
+    public $roles;
+
+    //Stores all the permissions
+    public $permissions;
+
+    //Handles the selected role
+    public $selectedRole;
+    
+    //Store the selected permission
+    public $selectedPermissions = [];
+    
+    //Handles the user creation state
+    public $confirmingUserCreate = false;
+    
+    //Stores the password input value
     public $password;
+
+    protected $listeners = ['loadSelectedRolePermissions' =>  'loadSelectedRolePermissions'];
 
     protected $rules = [ 
         'user.email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
@@ -23,6 +44,16 @@ class Create extends Component
 
     public function mount() {
         $this->user = new User(); 
+        $this->roles = Role::all();
+        $this->selectedRole  = $this->roles->first()->id; 
+        
+        $this->permissions = Permission::all();
+
+        $this->loadSelectedRolePermissions();
+    }
+
+    public function loadSelectedRolePermissions(){ 
+        $this->selectedPermissions = Role::findById($this->selectedRole)->permissions->pluck('id')->toArray();
     }
 
     public function confirmUserCreate() { 
@@ -31,32 +62,47 @@ class Create extends Component
         //Resets the field of the form 
         $this->user = new User();
         $this->password = null;
-
+        
+        $this->loadSelectedRolePermissions();
     }
-
+    
     public function submit() { 
-        if(filled($this->password)) { 
-            $this->validate([
-                'password' => ['required', 'min:8', 'max:255',  'string'],
-            ]);
-            
-            $this->user->password = bcrypt($this->password);
-            
-            $saved = $this->user->save();
 
-            if($saved) { 
-                //Refreshes the livewire datatable
-                $this->emit('refreshLivewireDatatable');
-                
-                //Closes the modal 
-                $this->confirmingUserCreate = false;
+        try { 
 
-                $this->dispatchBrowserEvent('success', [
-                    'type' => 'success',
-                    'title' => 'Success',
-                    'text' => 'User added successfully'
+            DB::beginTransaction();
+            if(filled($this->password)) { 
+                $this->validate([
+                    'password' => ['required', 'min:8', 'max:255',  'string'],
                 ]);
-            }
+                
+                $this->user->password = bcrypt($this->password);
+                
+                $saved = $this->user->save();
+
+                $this->user->assignRole($this->selectedRole);
+                $this->user->givePermissionTo($this->selectedPermissions);
+
+                if($saved) { 
+                    //Refreshes the livewire datatable
+                    $this->emit('refreshLivewireDatatable');
+                    
+                    //Closes the modal 
+                    $this->confirmingUserCreate = false;
+
+                    $this->dispatchBrowserEvent('success', [
+                        'type' => 'success',
+                        'title' => 'Success',
+                        'text' => 'User added successfully'
+                    ]);
+                }
+            }               
+            
+            DB::commit();
+
+        } catch(e) { 
+            DB::rollback();
         }
+ 
     }
 }
