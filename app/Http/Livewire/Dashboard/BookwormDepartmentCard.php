1123a90cadfe77b60d072f75ae11a8eebc376666
Michael Angelo De Leon <iammikedev@gmail.com>
2023-05-12 02:17:49 +0800

    refactors dashboard card

--- app/Http/Livewire/Dashboard/BookwormDepartmentCard.php
+++ app/Http/Livewire/Dashboard/BookwormDepartmentCard.php
@@ -7,7 +7,7 @@ use App\Models\StudentId;
 
 class BookwormDepartmentCard extends Component
 {
-    public StudentId $student_id  ; 
+    public StudentId $student_id ; 
 
     public function render()
     {
@@ -19,7 +19,7 @@ class BookwormDepartmentCard extends Component
     }
 
     public function getBookwormDepartment() { 
-        $this->student_id = StudentId::withCount('transactions')->orderBy('transactions_count', 'DESC')->first() ?? 0;
+        $this->student_id = StudentId::withCount('transactions')->orderBy('transactions_count', 'DESC')->first() ;
 
     }
 }
