f65ea468cb15314ef1e487bf8cfe307dfa49ecd2
Michael Angelo De Leon <iammikedev@gmail.com>
2023-05-18 01:41:23 +0800

    logs  reports viewer

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use OwenIt\Auditing\Models\Audit;
+use Carbon\Carbon;
 
 class ReportController extends Controller
 {
     //
     public function show() { 
+
+        $data = [
+            'auditable_id' => auth()->user()->id,
+            'auditable_type' => "App\Models\User",
+            'event'      => "viewed reports",
+            'url'        => request()->fullUrl(),
+            'ip_address' => request()->getClientIp(),
+            'user_agent' => request()->userAgent(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'user_id' => auth()->user()->id,
+        ];
+
+        //create audit trail data
+        Audit::create($data);
+
         return view('reports.show');
     }
 }
